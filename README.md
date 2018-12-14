74b29161d5fc45040981b949840fef25203fddc3
yehchge <yehchge@gmail.com>
2018-12-14 11:47:01 +0800

    Blog 的新增顯示修改刪除 CRUD

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # laravel_blog
+
+Ref: https://itsolutionstuff.com/post/laravel-57-crud-create-read-update-delete-tutorial-example-example.html
+
+
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('post', 'HomeController@index');
+// Route::get('post', 'HomeController@index');
+
+Route::resource('posts','HomeController');
